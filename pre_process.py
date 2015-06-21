48ed478a7ef01f57b62e57e865114662b9cedcba
Angad Gill <angadsinghgill@gmail.com>
2015-06-21 11:29:41 -0700

    pre_process now detects inverted images and flips them (up-down and right-left). it also detects right images and flips them right-to-left

--- pre_process.py
+++ pre_process.py
@@ -4,6 +4,33 @@ from os import walk, path
 import pandas as pd
 import numpy as np
 from scipy import misc
+import cv2
+
+
+def is_inverted_vert(image, filename):
+    # Output: True or False based on whether the image is 'inverted'
+    # as defined by the Kaggle contest on Diabetic Retinopath
+    # This function is based on Ravi's idea of testing
+    # to see if the bright spot is on the left or the
+    # right of the vertical center line
+    # Input: Expects images with equalized histograms as inpu
+    (r_max, c_max) = image.shape
+    maxval = image.max()
+    (t, image) = cv2.threshold(image, maxval-10, maxval, cv2.THRESH_BINARY)
+    (r, c) = image.nonzero()
+    if 'right' in filename:
+        if c.mean() > (c_max/2):
+            inverted = False
+        else:
+            inverted = True
+    elif 'left' in filename:
+        if c.mean() < (c_max/2):
+            inverted = False
+        else:
+            inverted = True
+    else:
+        inverted = None
+    return inverted
 
 
 def get_image_filenames(dir):
@@ -32,6 +59,11 @@ def extract(m, dimension):
     for (i, filename) in enumerate(filenames):
         #print i
         image = misc.imread(path.join(dir, filename), flatten=1)
+        if is_inverted_vert(image, filename):
+            image = np.fliplr(np.flipud(image))
+            print "Inverted image detected and flipped!"
+        if 'right' in filename:
+            image = np.fliplr(image)
         (r, c) = image.shape
         image = image.reshape(r*c)
         images += [image]
